34f53f10ed88f2faea08b8f3d1679c0f9d37b639
Lauren McCall <lmmccolloch@gmail.com>
2021-11-11 21:43:04 -0500

    initial commit

--- Program.cs
+++ Program.cs
@@ -2,11 +2,49 @@
 
 namespace VariablesCS
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Console.WriteLine("Welcome to C#");
+        static void Main(string[] args)
+        {
+
+            // practice creating variables
+            int numberOfCupsOfCoffee = 0;
+            string fullName = "Lauren McCall";
+            var today = DateTime.Now;
+            Console.WriteLine($"{fullName} has drank {numberOfCupsOfCoffee} cups of coffee as of {today}. ");
+
+            // practice getting input from the user
+            Console.Write("\n");
+            Console.Write("What is your name? ");
+            var userName = Console.ReadLine();
+            Console.WriteLine($"Hi {userName}! Welcome to this! ");
+
+            // practice getting different types of input from the user
+            Console.Write("\n");
+            Console.Write("Please input a number? ");
+            var firstNumberAsString = Console.ReadLine();
+            Console.Write("Please input another number? ");
+            var secondNumberAsString = Console.ReadLine();
+
+            // converting string input into numbers
+            double firstOperand = double.Parse(firstNumberAsString);
+            double secondOperand = double.Parse(secondNumberAsString);
+
+            // doing math
+            var sum = firstOperand + secondOperand;
+            var difference = firstOperand - secondOperand;
+            var product = firstOperand * secondOperand;
+            var quotient = firstOperand / secondOperand;
+            var remainder = firstOperand % secondOperand;
+
+            Console.WriteLine("\n");
+            Console.WriteLine("Sum: " + sum);
+            Console.WriteLine("Difference: " + difference);
+            Console.WriteLine("Product: " + product);
+            Console.WriteLine("Quotient: " + quotient);
+            Console.WriteLine("Remainder: " + remainder);
+            Console.WriteLine("\n");
+
+        }
     }
-  }
 }
